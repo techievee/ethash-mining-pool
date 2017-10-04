e6e118533bfaf7f9aaa3c67bafdac7ea334cc01c
TechieVee <techievee@gmail.com>
2017-10-04 18:35:36 +0530

    UBI 2.0 Compatible changes for the Pool Reward Fix of 250MC after block 1200000

--- payouts/unlocker.go
+++ payouts/unlocker.go
@@ -30,8 +30,12 @@ type UnlockerConfig struct {
 
 const minDepth = 10
 
-var constReward = math.MustParseBig256("314000000000000000000")
-var uncleReward = new(big.Int).Div(constReward, new(big.Int).SetInt64(32))
+var (
+	big2                 = big.NewInt(2)
+	big32                = big.NewInt(32)
+	BlockReward *big.Int = big.NewInt(250e+18)
+
+)
 
 // Donate 10% from pool fees to developers
 const donationFee = 10.0
@@ -209,14 +213,24 @@ func matchCandidate(block *rpc.GetBlockReply, candidate *storage.BlockData) bool
 }
 
 func (u *BlockUnlocker) handleBlock(block *rpc.GetBlockReply, candidate *storage.BlockData) error {
-	// Initial 314Music static reward
-	reward := new(big.Int).Set(constReward)
 
 	correctHeight, err := strconv.ParseInt(strings.Replace(block.Number, "0x", "", -1), 16, 64)
 	if err != nil {
 		return err
 	}
 	candidate.Height = correctHeight
+
+	// Rewards
+	reward := new(big.Int).Set(BlockReward)
+	headerNumber := big.NewInt(candidate.Height)
+
+    if headerNumber.Cmp(big.NewInt(1200000)) < 0 {
+        reward = big.NewInt(314e+18)
+    }
+
+
+    var uncleReward = new(big.Int).Div(reward, big32)
+
 
 	// Add TX fees
 	extraTxReward, err := u.getExtraRewardForTx(block)
@@ -542,7 +556,17 @@ func weiToShannonInt64(wei *big.Rat) int64 {
 }
 
 func getUncleReward(uHeight, height int64) *big.Int {
-	reward := new(big.Int).Set(constReward)
+
+
+    reward := new(big.Int).Set(BlockReward)
+    headerNumber := big.NewInt(height)
+
+    if headerNumber.Cmp(big.NewInt(1200000)) < 0 {
+        reward = big.NewInt(314e+18)
+    }
+
+
+	//reward := new(big.Int).Set(reward)
 	reward.Mul(big.NewInt(uHeight+8-height), reward)
 	reward.Div(reward, big.NewInt(8))
 	return reward
