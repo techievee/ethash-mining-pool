340f4c2e99acd7160ab2eb67f96ad62c28b0cfc5
notminerproduction <64098611+notminerproduction@users.noreply.github.com>
2020-04-21 23:29:11 +0300

    Add files via upload

---
+++ www/app/formats.js
@@ -0,0 +1,19 @@
+var hhmmss = {
+  hour:   'numeric',
+  minute: 'numeric',
+  second: 'numeric'
+};
+
+export default {
+  time: {
+    hhmmss: hhmmss
+  },
+  date: {
+    hhmmss: hhmmss
+  },
+  number: {
+    EUR: { style: 'currency', currency: 'EUR', minimumFractionDigits: 2, maximumFractionDigits: 2 },
+    USD: { style: 'currency', currency: 'USD', minimumFractionDigits: 2, maximumFractionDigits: 2 },
+    INR: { style: 'currency', currency: 'INR', minimumFractionDigits: 0, maximumFractionDigits: 2 ,  useGrouping: false }
+  }
+};
